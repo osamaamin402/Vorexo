da79e181bc06c4304fd62b9e923dbde5e3db6a5d
osamaamin402 <osamaamin402@gmail.com>
2025-02-23 11:32:24 +0530

    first commit

---
+++ src/components/partners.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Partners = (props) => {
+  return (
+    <div id="partners" className="text-center">
+      <div className="container">
+        <div className="section-title text-center">
+          <h2>Our Partners</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
