ed3ba1ebb68cfda9627c6048be2aadb81e5e046f
osamaamin402 <osamaamin402@gmail.com>
2025-02-28 10:10:43 +0530

    data

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,11 +5,11 @@ export const About = (props) => {
     <div id="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6">
+          <div className="col-xs-12 col-md-5">
             {" "}
             <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
           </div>
-          <div className="col-xs-12 col-md-6">
+          <div className="col-xs-12 col-md-7">
             <div className="about-text">
               <h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
